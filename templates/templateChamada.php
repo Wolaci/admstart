5d670a7dfa7f47fe20f480706866445cd0cbd889
Wolaci <wolacijose8@gmail.com>
2021-07-26 18:53:21 -0300

    Criando pasta template

---
+++ templates/templateChamada.php
@@ -0,0 +1,16 @@
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="assets/css/all.min.css">
+<!-- icheck bootstrap -->
+<link rel="stylesheet" href="assets/css/icheck-bootstrap.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="assets/css/adminlte.min.css">
+<!-- jQuery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/js/adminlte.min.js"></script>
+<!-- jQuery Mask-->
+<script src="assets/js/jquery.mask.min.js"></script>
