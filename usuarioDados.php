826cd0b17fa6944f9ffc98bd509eb72f204e88cd
Wolaci <wolacijose8@gmail.com>
2021-07-29 20:29:33 -0300

    Criando insert

--- usuarioDados.php
+++ usuarioDados.php
@@ -1,6 +1,12 @@
 <?php
 
-var_dump($_POST);
+include 'includes/conn.php';
 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = ($_POST['senha'] !='') ? base64_encode($_POST['senha']) : '';
+
+$sql = $conn->prepare("INSERT INTO usuarios (nome , email, senha, status) VALUES (?, ?, ?, ?)");
+$sql->execute([$nome,$email,$senha,'SIM']);
 
 ?>
